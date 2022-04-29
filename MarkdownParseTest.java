9db507343a8230057e46d8a4e3c68cddcece3309
Khiem Dang <103155859+khiemddang@users.noreply.github.com>
2022-04-29 09:24:41 -0700

    Update MarkdownParseTest.java
    Added failure-inducing output test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -32,6 +32,19 @@ public class MarkdownParseTest {
 
         assertEquals(expected, toReturn);
     }
+
+    @Test
+    public void test3() throws IOException {
+        Path fileName = Path.of("test-file3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> toReturn = MarkdownParse.getLinks(content);
+
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://something.com");
+        expected.add("some-thing.html");
+
+        assertEquals(expected, toReturn);
+    }
 }
 
 
